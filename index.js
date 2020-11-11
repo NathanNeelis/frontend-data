bf0d43fab252c56f6ecbf45e94c387d35077f87b
NathanNeelis <nathan.neelis@hva.nl>
2020-11-11 18:36:26 +0100

    some details

--- index.js
+++ index.js
@@ -1,146 +1,149 @@
 const endpointNPR = 'https://gist.githubusercontent.com/NathanNeelis/b28e16c0433b12da6bc716b276901ae9/raw/402754fa45924af802d06c5672043153bb990d5b/NPR_park_and_ride';
 
 import {
-  			combineDoubleCities, 
-  			cleaningData
-				} from './transform';
+  combineDoubleCities,
+  cleaningData
+} from './transform';
 
-import { setupScales, 
-        setupAxis, 
-        drawBars,
-        yScale,
-        xScale,
-        xValue,
-        yValue,
-        g,
-        margin,
-        innerHeight,
-        innerWidth,
-        svg,
-        height,
-        width
-       } from './visual'
+import {
+  setupScales,
+  setupAxis,
+  drawBars,
+  yScale,
+  xScale,
+  xValue,
+  yValue,
+  g,
+  margin,
+  innerHeight,
+  innerWidth,
+  svg,
+  height,
+  width
+} from './visual'
 
-import { select,  
-        scaleLinear, 
-        max, 
-        scaleBand, 
-        axisLeft,
-       	axisBottom,
-       	format,
-        nice
-       } from 'd3';
+import {
+  select,
+  scaleLinear,
+  max,
+  scaleBand,
+  axisLeft,
+  axisBottom,
+  format,
+  nice
+} from 'd3';
 
 let prDataSet
 
 makeVisualization()
-async function makeVisualization(){
+async function makeVisualization() {
   getData(endpointNPR)
-      .then(data => {
-    
-    	let prData = cleaningData(data);
+    .then(data => {
+
+      let prData = cleaningData(data);
       let combine = combineDoubleCities(prData);
 
       console.log('all P+R', data)
       console.log('P+R for each city', combine);
-    
-    	prDataSet = cleaningData(data)
-    	
-    	setupScales(prData) // import from visual
-    	setupAxis(prData) // import from visual
-    	drawBars(prData) // import from visual
-    	setupInput(prData) // not an import 
-  });
+
+      prDataSet = cleaningData(data)
+
+      setupScales(prData) // import from visual
+      setupAxis(prData) // import from visual
+      drawBars(prData) // import from visual
+      setupInput(prData) // not an import 
+    });
 }
 
 async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-function setupInput(){
- // &CREDITS code example by Laurens
+function setupInput() {
+  // &CREDITS code example by Laurens
   const input = select('#filter')
-      // .on("click", changeOutput)
-  		// .on("click", () => changeOutput(data));
-  		.on('click', changeOutput)
+    // .on("click", changeOutput)
+    // .on("click", () => changeOutput(data));
+    .on('click', changeOutput)
 }
 
-function changeOutput(){
-	const filterOn = this? this.checked : false;
+function changeOutput() {
+  const filterOn = this ? this.checked : false;
   console.log('checkbox', this.checked)
-  
+
   // const dataSelection = filterOn? prDataSet.filter(parking => parking.capacity) : data
-  const dataSelection = filterOn? combineDoubleCities(prDataSet) : prDataSet
+  const dataSelection = filterOn ? combineDoubleCities(prDataSet) : prDataSet
   console.log('new data', dataSelection)
-  
+
   //Update the domains
   yScale
-  .domain([max(dataSelection, yValue), 0])
-  .nice()
+    .domain([max(dataSelection, yValue), 0])
+    .nice()
 
   xScale
-  .domain(dataSelection.map(xValue))
+    .domain(dataSelection.map(xValue))
 
-  
+
   //Bars will store all bars created so far
   //$CREDITS ==  Code example by LAURENS 
   const bars = g.selectAll('.bar')
-  	.data(dataSelection)
-  
-  
+    .data(dataSelection)
+
+
   // update
   bars
-  	.attr('y', d => yScale(yValue(d)))
+    .attr('y', d => yScale(yValue(d)))
     .attr('x', d => xScale(xValue(d)))
-  	.attr('width', xScale.bandwidth())
-  
-    .attr("y", function(d) {
-    	return yScale(0);
-    	})
-  	.attr("height", 0)
-  	.transition().duration(1000)
-  	.attr('y', d => yScale(yValue(d)))
-  	.attr('height', d => innerHeight - yScale(yValue(d)))
+    .attr('width', xScale.bandwidth())
+
+    .attr("y", function (d) {
+      return yScale(0);
+    })
+    .attr("height", 0)
+    .transition().duration(1000)
+    .attr('y', d => yScale(yValue(d)))
+    .attr('height', d => innerHeight - yScale(yValue(d)))
   // console.log('data at update point', dataSelection)
- 
-  
+
+
   //Enter
   bars.enter()
-  		.append('rect')
-  			.attr('class', 'bar')
-  			.attr('x', d => xScale(xValue(d)))
-  			.attr('y', d => yScale(yValue(d)))
-  			.attr('width', xScale.bandwidth())
-      
-    	.attr("y", function(d) {
-    	return yScale(0);
-    	})
-      .attr("height", 0)
-      .transition().duration(1000)
-      .attr('y', d => yScale(yValue(d)))
-      .attr("height", d => innerHeight - yScale(yValue(d)));
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => xScale(xValue(d)))
+    .attr('y', d => yScale(yValue(d)))
+    .attr('width', xScale.bandwidth())
 
-  		// RESOURCE BARS FROM BOTTOM TO TOP:
-  		// RESOURCE: https://stackoverflow.com/questions/36126004/height-transitions-go-from-top-down-rather-than-from-bottom-up-in-d3
-  
+    .attr("y", function (d) {
+      return yScale(0);
+    })
+    .attr("height", 0)
+    .transition().duration(1000)
+    .attr('y', d => yScale(yValue(d)))
+    .attr("height", d => innerHeight - yScale(yValue(d)));
+
+  // RESOURCE BARS FROM BOTTOM TO TOP:
+  // credits for Harry Vane @ stackoverflow
+  // RESOURCE: https://stackoverflow.com/questions/36126004/height-transitions-go-from-top-down-rather-than-from-bottom-up-in-d3
+
   //Exit
   bars.exit()
-  		.remove()
+    .remove()
 
   //Update the ticks	
-    svg.select('.axis-x')
-      .call(axisBottom(xScale))
-    	.attr('transform', `translate(0, ${innerHeight})`)
-  		.selectAll("text")
-          .attr("transform", `rotate(50)`)
-  				.attr('text-anchor', 'start')
-          .attr('x', 10)
-  				.attr('y', 5)
-  	svg.select('.axis-y')
-      .call(axisLeft(yScale).tickSize(-innerWidth))
-			  .selectAll('.domain') // removing Y axis line and ticks
-  			.remove()
-  	
+  svg.select('.axis-x')
+    .call(axisBottom(xScale))
+    .attr('transform', `translate(0, ${innerHeight})`)
+    .selectAll("text")
+    .attr("transform", `rotate(50)`)
+    .attr('text-anchor', 'start')
+    .attr('x', 10)
+    .attr('y', 5)
+  svg.select('.axis-y')
+    .call(axisLeft(yScale).tickSize(-innerWidth))
+    .selectAll('.domain') // removing Y axis line and ticks
+    .remove()
+
 }
