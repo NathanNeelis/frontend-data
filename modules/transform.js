cf3b0e24d986c0e1d0f758daecdbbe6f7c994b14
NathanNeelis <nathan.neelis@hva.nl>
2020-11-11 15:53:37 +0100

    code from vizhub added. Working visual

--- modules/transform.js
+++ modules/transform.js
@@ -1,85 +1,22 @@
-const endpointNPR = 'https://gist.githubusercontent.com/NathanNeelis/b28e16c0433b12da6bc716b276901ae9/raw/402754fa45924af802d06c5672043153bb990d5b/NPR_park_and_ride';
-
-getData(endpointNPR)
-    .then(prData => {
-        console.log('all Park & ride data:', prData);
-
-        let prCapacity = getCapacity(prData);
-        // console.log('All Capacities', prCapacity)
-
-        // FIX THIS ONE -- to realy citiname
-        let prCityArray = filterCity(prData);
-        // console.log('all cities', prCityArray);
-
-        let prDescription = filterData(prData, 'description')
-        // console.log('descriptions', prDescription);
-
-        let objectArray = wrap(prCityArray, prCapacity, prDescription);
-        console.log('new object', objectArray);
-
-        // let tarifArray = getRate(prData); // RATES TO DO LATER
-
-        // ARRAY WITH DATA OBJECT FOR RANDSTAND CITIES
-        // randstad cities; Delft, Dordrecht, Leiden, Zaanstad, Haarlem, Utrecht, Den Haag, Rotterdam, Amsterdam
-        let randstadCities = selectRandstad(objectArray);
-        console.log('testing randstad cities', randstadCities);
-
-        // clean the randstad cities.
-        // combine the data if city name is the same and add capacity.
-        let randstadClean = cleanRandStadData(randstadCities);
-        console.log('randstad cities clean', randstadClean)
-
-        let ranstadWithoutDoubles = listUnique(randstadClean);
-        console.log('without doubles', ranstadWithoutDoubles);
-
-        let combine = combineDoubleCities(randstadClean);
-        console.log('randstad fixed', combine);
-
-
-    })
-
-async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+export function cleaningData(data){
+      let prCapacity = getCapacity(data);
+      let prCityArray = filterCity(data);
+      let prDescription = filterData(data, 'description');
+      let objectArray = wrap(prCityArray, prCapacity, prDescription);
+      let randstadCities = selectRandstad(objectArray);
+      let randstadClean = cleanRandStadData(randstadCities);
+  		let cleanData = listUnique(randstadClean)
+  return cleanData
 }
 
-function combineDoubleCities(rsData) {
-
-    // let combinedData = rsData.map((data) => {
-    //     if (data.city === data.city) {
-    //         return {
-    //             city: data.city,
-    //             capacity: data.capacity + data.capacity
-    //         }
-
-    //     }
-    // })
-    // console.log('test test test', combinedData)
-
-
-    // let combinedData = obj.reduce((a, v) => {
-    //     if (a[v.city]) {
-    //         a[v.city].capacity = a[v.city].capacity + v.capacity
-    //         // console.log('andere data', a[v.city].capacity = a[v.city].capacity + v.capacity)
-    //     } else {
-    //         a[v.city] = v
-    //         // console.log('dit is v', v)
-    //     }
-    //     // console.log('dit is a', a)
-    //     return a
-
-    // }, [])
-    // console.log('test combining', combinedData);
-    // return combinedData
-
+export function combineDoubleCities(rsData) {
     let arr = rsData,
         result = [];
 
     arr.forEach(function (a) {
         if (!this[a.city]) {
             this[a.city] = {
-                description: a.description,
+                description: a.city,
                 city: a.city,
                 capacity: 0
             };
@@ -89,13 +26,13 @@ function combineDoubleCities(rsData) {
     }, Object.create(null));
 
     return result;
-
-    // WINNING RESOURCE: https://stackoverflow.com/questions/38294781/how-to-merge-duplicates-in-an-array-of-objects-and-sum-a-specific-property
-    // RESOURCE: https://stackoverflow.com/questions/60036060/combine-object-array-if-same-key-value-in-javascript
+  
 }
 
+
+
 // returns array of objects with the right citynames
-function cleanRandStadData(rsData) {
+export function cleanRandStadData(rsData) {
     let delftClean = combineData(rsData, 'Delft');
     let dordrechtClean = combineData(delftClean, 'Dordrecht');
     let leidenClean = combineData(dordrechtClean, 'Leiden');
@@ -111,15 +48,12 @@ function cleanRandStadData(rsData) {
     let fixedCleanData = fixDescription(cleanData);
 
     return fixedCleanData;
-
+    // console.log('fixed cities in randstad', amsterdamClean);
 }
 
 
-
-
-
 // returns an array with all data for ranstad cities
-function selectRandstad(objectArray) {
+export function selectRandstad(objectArray) {
 
     let delftCities = filterRandstad(objectArray, 'Delft');
     let dordrechtCities = filterRandstad(objectArray, 'Dordrecht');
@@ -140,7 +74,7 @@ function selectRandstad(objectArray) {
 }
 
 // returns an array of all the capicity for each parking area
-function getCapacity(prData) {
+export function getCapacity(prData) {
     let prSpecifications = filterData(prData, 'specifications')
     // console.log('all specifications', prSpecifications);
 
@@ -157,7 +91,7 @@ function getCapacity(prData) {
 }
 
 // Returns an array of all cities
-function filterCity(prData) {
+export function filterCity(prData) {
     // FIRST CITY NAME
     let accessPointDataArray = filterData(prData, 'accessPoints');
     let accessPointDataArrayClean = removeOuterArray(accessPointDataArray);
@@ -183,21 +117,9 @@ function filterCity(prData) {
     return cityNameObject
 }
 
-// // RATES FOR PR AREAS
-// function getRate(prData) {
-//     let tariffsData = filterData(prData, 'tariffs')
-//     console.log('tarifs Data', tariffsData)
-
-//     let tariffsDataClean = wrapRates(tariffsData);
-//     console.log('found rate', tariffsDataClean);
-
-//     console.log('finding rate', tariffsData[0][0].intervalRates[0].charge)
-
-
-// }
 
 // FILTER DATA ON RANDSTAD CITIES
-function filterRandstad(prData, city) {
+export function filterRandstad(prData, city) {
     let randstadData = prData.filter(array => {
         return array.cityFirst === city || array.citySecond === city;
     })
@@ -206,17 +128,17 @@ function filterRandstad(prData, city) {
 
 
 // returns an array of all data in a specific column
-function filterData(dataArray, column) {
+export function filterData(dataArray, column) {
     return dataArray.map(result => result[column]);
 }
 
 
 // removes outer array
-function removeOuterArray(prData) {
+export function removeOuterArray(prData) {
     return prData.map(result => result[0]);
 }
 
-function fixEmptyValues(prData) {
+export function fixEmptyValues(prData) {
     // Create an object with all the keys in it
     // This will return one object containing all keys the items
     let obj = prData.reduce((res, item) => ({
@@ -252,17 +174,17 @@ function fixEmptyValues(prData) {
 
 
 // removes all items in array that do not have the key "name" in the object.
-function removeNoName(allData) {
+export function removeNoName(allData) {
     let newArray = allData.filter(obj => Object.keys(obj).includes("name"));
     return newArray
 }
 // Resource: https://stackoverflow.com/questions/51367551/how-to-remove-object-from-array-if-property-in-object-do-not-exist
 
 
-function wrap(city, capacity, description) {
+export function wrap(city, capacity, description) {
     let items = city.map((city, index) => {
         return {
-            description: description[index],
+          	description: description[index],
             cityFirst: city.cityFirst,
             citySecond: city.citySecond,
             capacity: capacity[index]
@@ -272,7 +194,7 @@ function wrap(city, capacity, description) {
     return items
 }
 
-function wrapCity(cityNameOne, cityNameTwo) {
+export function wrapCity(cityNameOne, cityNameTwo) {
     let cities = cityNameOne.map((city, index) => {
         return {
             cityFirst: city,
@@ -289,7 +211,8 @@ function wrapCity(cityNameOne, cityNameTwo) {
 // if so then it changes the object to city: city.
 // instead of cityFirst and citySecond.
 // It also checks if there already is a key "city" that contains data
-function combineData(rsData, city) {
+
+export function combineData(rsData, city) {
     let cleanData = rsData.map((data) => {
         if (data.cityFirst === city || data.citySecond === city) {
             // console.log('i am looking for', city)
@@ -315,9 +238,10 @@ function combineData(rsData, city) {
 
     // DIT KAN WAARSCHIJNLIJK NOG WEL MOOIER MET EEN ARRAY VAN RANDSTAD CITIES DIE HIER LOOPT. MISSCHIEN VOOR LATER.
 }
+  
 
 // If there is a undefined description it returns the description as 'P+R' + the city name
-function fixDescription(rsData) {
+export function fixDescription(rsData) {
     let cleanData = rsData.map((data) => {
         if (data.description === undefined) { // if description is undefined, change description to P+R + cityname
             return {
